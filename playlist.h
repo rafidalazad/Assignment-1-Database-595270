016cb3106df32289b4c852cdb813f265127de04f
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2023-10-20 18:50:10 +1100

    Update playlist.h

--- playlist.h
+++ playlist.h
@@ -1,25 +1,26 @@
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable:4996)
-
 #pragma once
-#ifndef PLAYLIST_H
-#define PLAYLIST_H
 
+// Necessary system headers are included
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definition of the Song structure
 typedef struct Song {
-    char name[100];
+    char *name; // dynamic string
     struct Song* next;
 } Song;
 
+// Definition of the Playlist structure
 typedef struct Playlist {
-    char name[100];
+    char *name; // dynamic string
     Song* songs;
     struct Playlist* next;
 } Playlist;
 
+// Function declarations for playlist operations
 void add_song(Playlist* playlist, const char* song_name);
 void remove_song(Playlist* playlist, const char* song_name);
 float calculate_similarity(Playlist* playlist1, Playlist* playlist2);
 void free_playlists(Playlist* playlists);
 void print_playlists(Playlist* playlists);
-
-#endif
